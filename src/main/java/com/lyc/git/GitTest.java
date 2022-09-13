ba2d6b9415fb280722e0ff608b9c613cdb600ff1
Asin <Asin@qq.com>
2022-09-13 16:39:35 +0800

    hot-fix commit-1

--- src/main/java/com/lyc/git/GitTest.java
+++ src/main/java/com/lyc/git/GitTest.java
@@ -2,8 +2,9 @@ package com.lyc.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
         System.out.println("hello git! 1");
         System.out.println("hello git! 2");
+        System.out.println("hello git! 3");
+        System.out.println("hello git! 4");
     }
 }
